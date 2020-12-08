5c6f0c615e767aa19931cea9facd1330d2d4d70a
Greg <g.rutherford10@gmail.com>
2020-12-08 19:45:41 +0000

    start of basic app, fetch data into state with axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import "./App.css";
+import React, { Component } from "react";
+import axios from "axios";
+import "./stylesheet.scss";
 
-function App() {
-	return (
-		<div className="app-container">
-			<h2>Space-X Launches</h2>
-		</div>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			launches: [],
+		};
+	}
+
+	componentDidMount() {
+		axios("https://api.spacexdata.com/v4/launches/")
+			.then((res) =>
+				this.setState({
+					launches: res.data,
+				})
+			)
+			.catch((err) => console.error(err));
+	}
+
+	render() {
+		return (
+			<div className="app-container">
+				<h2>Space-X Launches</h2>
+			</div>
+		);
+	}
 }
 
 export default App;
